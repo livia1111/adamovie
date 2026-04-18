d48bd44c1b99054dd2fad22ee0b0f8382680a473
Afranio Caires <afraniomcaires@gmail.com>
2026-04-17 22:16:29 -0300

    feat: add modulo de menu

--- src/main/java/org/adafilmes/Ator.java
+++ src/main/java/org/adafilmes/Ator.java
@@ -1,20 +1,13 @@
 package org.adafilmes;
 
-import java.time.LocalDate;
-
 public class Ator extends Pessoa {
-    double cache;
 
-    public Ator(String nome, LocalDate dataNascimento, String nacionalidade, double cache) {
-        super(nome,dataNascimento,nacionalidade);
-        this.cache = cache;
+    public Ator(String nome) {
+        super(nome);
     }
 
-    public double getCache() {
-        return cache;
-    }
-
-    public void setCache(double cache) {
-        this.cache = cache;
+    @Override
+    public String toString() {
+        return "Ator: " + getNome();
     }
 }
--- src/main/java/org/adafilmes/Catalogo.java
+++ src/main/java/org/adafilmes/Catalogo.java
@@ -1,5 +1,65 @@
 package org.adafilmes;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class Catalogo {}
+public class Catalogo {
+    private List<Filme> filmes;
+    private List<Ator> atores;
+    private List<Diretor> diretores;
+
+    public Catalogo() {
+        this.filmes = new ArrayList<>();
+        this.atores = new ArrayList<>();
+        this.diretores = new ArrayList<>();
+    }
+
+    public void cadastrarFilme(Filme filme) {
+        filmes.add(filme);
+    }
+
+    public void cadastrarAtor(Ator ator) {
+        atores.add(ator);
+    }
+
+    public void cadastrarDiretor(Diretor diretor) {
+        diretores.add(diretor);
+    }
+
+    public void associarDiretorFilme(int filmeId, Diretor diretor) {
+        for (Filme filme : filmes) {
+            if (filme.getId() == filmeId) {
+                filme.setDiretor(diretor);
+                break;
+            }
+        }
+    }
+
+    public void associarAtorFilme(int filmeId, Ator ator) {
+        for (Filme filme : filmes) {
+            if (filme.getId() == filmeId) {
+                filme.adicionarAtor(ator);
+                break;
+            }
+        }
+    }
+
+    public List<Filme> buscarFilmePorNome(String nome) {
+        return filmes.stream()
+                .filter(f -> f.getNome().toLowerCase().contains(nome.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Filme> getFilmes() {
+        return filmes;
+    }
+
+    public List<Ator> getAtores() {
+        return atores;
+    }
+
+    public List<Diretor> getDiretores() {
+        return diretores;
+    }
+}
